fef139cf6badcafa19ede5ea2d461e00aa19d4c8
Skpone <mulaterofranco@gmail.com>
2023-10-31 18:07:03 -0300

    generator

---
+++ src/main/java/com/example/integrador3arqui/Integrador3ArquiApplication.java
@@ -0,0 +1,13 @@
+package com.example.integrador3arqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Integrador3ArquiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Integrador3ArquiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/integrador3arqui/Integrador3ArquiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.integrador3arqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Integrador3ArquiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
