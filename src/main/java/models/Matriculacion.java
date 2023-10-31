5d75af4dc227df9b84029b411ed513b594d42e5d
rom-L <larrondoromeo@gmail.com>
2023-10-31 18:55:19 -0300

    .

---
+++ src/main/java/models/Matriculacion.java
@@ -0,0 +1,37 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@RequiredArgsConstructor @Getter
+@Table(name = "matriculaciones")
+public class Matriculacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "estudiante_dni")
+    private static Estudiante estudiante;
+
+    @ManyToOne
+    @JoinColumn(name = "carrera_id")
+    private static Carrera carrera;
+
+    @Column(name = "anio_inscripcion")
+    private static int anioInscripcion;
+
+    @Column(name = "anio_graduacion", nullable = true)
+    private static Integer anioGraduacion;
+
+    @Column(name = "antiguedad")
+    private static int antiguedad;
+
+}
+
+
+
+
