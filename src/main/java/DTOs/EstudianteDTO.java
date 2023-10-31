9ac0424d0ce8a071a02bf23d53aeeab45fd8d19c
rom-L <larrondoromeo@gmail.com>
2023-10-31 20:05:33 -0300

    fix

--- src/main/java/DTOs/EstudianteDTO.java
+++ src/main/java/DTOs/EstudianteDTO.java
@@ -1,9 +1,30 @@
 package DTOs;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor @Getter
+@Getter
 public class EstudianteDTO {
+    private Long dni;
+    private String nombres;
+    private String apellido;
+    private int edad;
+    private String genero;
+    private String ciudadResidencia;
+    private String numeroLibreta;
 
+    public EstudianteDTO(Long dni, String nombres, String apellido, int edad, String genero, String ciudadResidencia, String numeroLibreta) {
+        this.dni = dni;
+        this.nombres = nombres;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.genero = genero;
+        this.ciudadResidencia = ciudadResidencia;
+        this.numeroLibreta = numeroLibreta;
+    }
+
+
+    @Override
+    public String toString() {
+        return "DNI: " + this.dni + " - " + this.nombres + " " + this.apellido + " | Edad: " + this.edad + " | Genero: " + this.genero + " | Ciudad: " + this.ciudadResidencia + " | Nro Libreta: " + this.numeroLibreta;
+    }
 }
