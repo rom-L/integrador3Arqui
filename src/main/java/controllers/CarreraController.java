3f36db0c7b55bb4bd82381b7965de09ce703aa37
rom-L <larrondoromeo@gmail.com>
2023-10-31 20:44:21 -0300

    carrera

--- src/main/java/controllers/CarreraController.java
+++ src/main/java/controllers/CarreraController.java
@@ -1,9 +1,23 @@
 package controllers;
 
+import DTOs.CarreraDTO;
+import models.Carrera;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import services.CarreraService;
 
 @RestController
-@RequestMapping("carreras/")
+@RequestMapping("/carreras")
 public class CarreraController {
+    @Autowired
+    private CarreraService carreraService;
+
+
+    @PostMapping("")
+    public CarreraDTO post(@RequestBody Carrera carrera) {
+        return this.carreraService.save(carrera);
+    }
 }
--- src/main/java/services/CarreraService.java
+++ src/main/java/services/CarreraService.java
@@ -1,7 +1,24 @@
 package services;
 
+import DTOs.CarreraDTO;
+import models.Carrera;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import repositories.CarreraRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CarreraService {
+    @Autowired
+    private CarreraRepository carreraRepository;
+
+
+    public CarreraDTO save(Carrera carrera) {   /**INSERTA UNA CARRERA**/
+        Carrera carreraGuardada = this.carreraRepository.save(carrera);
+
+        //retorna un CarreraDTO copiando los atributos de la carrera guardada
+        return new CarreraDTO(carreraGuardada.getId(), carreraGuardada.getNombre());
+    }
 }
