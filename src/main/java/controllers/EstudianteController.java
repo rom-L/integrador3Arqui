3a1545eda41d280a291b4550768e6c288fe0d9cf
rom-L <larrondoromeo@gmail.com>
2023-10-31 19:28:52 -0300

    cambios

--- src/main/java/controllers/EstudianteController.java
+++ src/main/java/controllers/EstudianteController.java
@@ -1,9 +1,36 @@
 package controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import DTOs.EstudianteDTO;
+import models.Estudiante;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import services.EstudianteService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/estudiantes")
 public class EstudianteController {
+    @RestController
+    @RequestMapping("/estudiantes")
+    public class EstudianteController {
+        @Autowired
+        private EstudianteService estudianteService;
+
+
+        @GetMapping("")
+        public List<EstudianteDTO> getAll() {
+            return this.estudianteService.findAll();
+        }
+
+        @GetMapping("{id}")
+        public EstudianteDTO getById(@PathVariable Long id) {
+            return this.estudianteService.findById(id);
+        }
+
+        @PostMapping("")
+        public EstudianteDTO post(@RequestBody Estudiante estudiante) {
+            return this.estudianteService.save(estudiante);
+        }
+    }
 }
--- src/main/java/services/EstudianteService.java
+++ src/main/java/services/EstudianteService.java
@@ -1,7 +1,51 @@
 package services;
 
+import DTOs.EstudianteDTO;
+import models.Estudiante;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import repositories.EstudianteRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class EstudianteService {
+    @Autowired
+    private EstudianteRepository estudianteRepository;
+
+
+    public List<EstudianteDTO> findAll() {
+        List<Estudiante> resultado = this.estudianteRepository.findAll();   //resultado de la query
+        List<EstudianteDTO> listaNueva = new ArrayList<>(); //lista vacia para poner los DTOs
+
+        for (Estudiante estudiante : resultado) {
+            EstudianteDTO estudianteDTO = new EstudianteDTO(estudiante.getDni(), estudiante.getNombres(),
+                    estudiante.getApellido(), estudiante.getEdad(), estudiante.getGenero(),
+                    estudiante.getCiudadResidencia(), estudiante.getNumeroLibreta());
+
+
+            listaNueva.add(estudianteDTO);
+        }
+
+        return listaNueva;  //retorna la lista de DTOs
+    }
+
+    public EstudianteDTO findById(Long id) {
+        Estudiante estudianteResultado = this.estudianteRepository.findById(id).get();   //se usa el .get() para conseguir el Estudiante del Optional
+
+        //retorna un EstudianteDTO copiando los atributos del estudiante resultado
+        return new EstudianteDTO(estudianteResultado.getDni(), estudianteResultado.getNombres(),
+                estudianteResultado.getApellido(), estudianteResultado.getEdad(), estudianteResultado.getGenero(),
+                estudianteResultado.getCiudadResidencia(), estudianteResultado.getNumeroLibreta());
+    }
+
+    public EstudianteDTO save(Estudiante estudiante) {
+        Estudiante estudianteGuardado = this.estudianteRepository.save(estudiante);
+
+        //retorna un EstudianteDTO copiando los atributos del estudiante guardado
+        return new EstudianteDTO(estudianteGuardado.getDni(), estudianteGuardado.getNombres(),
+                estudianteGuardado.getApellido(), estudianteGuardado.getEdad(), estudianteGuardado.getGenero(),
+                estudianteGuardado.getCiudadResidencia(), estudianteGuardado.getNumeroLibreta());
+    }
 }
