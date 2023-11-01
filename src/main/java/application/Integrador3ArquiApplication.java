5f0071474053598a9856503eb6497b52376bf35b
rom-L <larrondoromeo@gmail.com>
2023-11-01 17:48:55 -0300

    asdasd

--- src/main/java/application/Integrador3ArquiApplication.java
+++ src/main/java/application/Integrador3ArquiApplication.java
@@ -1,4 +1,4 @@
-package com.example.integrador3arqui;
+package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
