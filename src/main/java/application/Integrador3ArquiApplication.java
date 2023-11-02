ce9c198b136c1951ab210178012f470cc92d1828
Skpone <mulaterofranco@gmail.com>
2023-11-02 19:31:01 -0300

    implementacion csvs

--- src/main/java/application/Integrador3ArquiApplication.java
+++ src/main/java/application/Integrador3ArquiApplication.java
@@ -2,18 +2,25 @@ package application;
 
 import application.util.CargaDeDatos;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class Integrador3ArquiApplication {
 
+    @Autowired
+    private CargaDeDatos cargaDeDatos;
+
     public static void main(String[] args) {
-
         SpringApplication.run(Integrador3ArquiApplication.class, args);
+    }
 
-        @PostConstruct
-        CargaDeDatos.run;
+    @PostConstruct
+    public void init() throws IOException {
+        cargaDeDatos.cargarDatosDesdeCSV();
     }
 
 }
